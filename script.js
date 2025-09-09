6f2e0c8036e713e4a5ea21c08fe200d2fd15630d
Agustin <agusgodoyds@gmail.com>
2025-09-09 19:13:45 -0300

    Agregando Productos

---
+++ script.js
@@ -0,0 +1,15 @@
+// str="Rocio Te amo "
+// Number=18
+// Boolean=true
+// - **let**: para datos que pueden cambiar.
+// - **const**: para datos que no deben modificarse después de su inicializació
+// // let (vaariable)para defininirla
+// // let numero = 20 para  darle valor
+// // definir luego inicializar
+// let numero = 29; defino/inicializo
+
+// bienvenida="Hola Bienvenido a la pagina de Doble ZZ";
+
+// console.log(bienvenida)
+let nombre=prompt("Ingrese Su Nombre")
+alert("Hola "+nombre+ " Espero que te encuentres bien en la pagina de doble zz")
