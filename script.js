4479f3820eb2438bfe96078e7038a814563b6023
Agustin <agusgodoyds@gmail.com>
2025-10-04 11:56:55 -0300

    Agregando productos y logica del ecommerce

--- script.js
+++ script.js
@@ -1,15 +1,582 @@
-// str="Rocio Te amo "
-// Number=18
-// Boolean=true
-// - **let**: para datos que pueden cambiar.
-// - **const**: para datos que no deben modificarse después de su inicializació
-// // let (vaariable)para defininirla
-// // let numero = 20 para  darle valor
-// // definir luego inicializar
-// let numero = 29; defino/inicializo
+const productos=[{
+    id:1,
+    nombre:"Boca juniors Conjunto",
+    categoria: "Primera Division",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+},
+{
+    id:2,
+    nombre:"Boca Juniors Mangalarga",
+    categoria: "Primera Division",
+    estilo:"Mangalarga",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+},
+{
+    id:3,
+    nombre:"Boca Juniors Conjunto Quilmes",
+    categoria: "Primera Division",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+},
+{
+    id:4,
+    nombre:"Boca Juniors Mangalarga azul",
+    categoria: "Primera Division",
+    estilo:"Mangalarga",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+},
+{
+    id:5,
+    nombre:"River Mangalarga",
+    categoria: "Primera Division",
+    estilo:"Mangalarga",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+},
+{
+    id:6,
+    nombre:"River Plate Conjunto Alternativo",
+    categoria: "Primera Division",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+},
+{
+    id:7,
+    nombre:"River Plate Mangalarga blanca",
+    categoria: "Primera Division",
+    estilo:"Mangalarga",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+},
+{
+    id:8,
+    nombre:"River Plate Conjunto Quilmes",
+    categoria: "Primera Division",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+},
+{
+    id:9,
+    nombre:"Independiente Conjunto",
+    categoria: "Primera Division",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+},
+{
+    id:10,
+    nombre:"Central Conjunto",
+    categoria: "Primera Division",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+},
+{
+    id:11,
+    nombre:"Central Alternativo Conjunto",
+    categoria: "Primera Division",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+},
+{
+    id:12,
+    nombre:"Central Cordoba Conjunto",
+    categoria: "Primera Division",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+},
+{
+    id:13,
+    nombre:"Central Cordoba Mangalarga",
+    categoria: "Primera Division",
+    estilo:"Mangalarga",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+},
+{
+    id:14,
+    nombre:"Defensa y Justicia Conjunto",
+    categoria: "Primera Division",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+},
+{
+    id:15,
+    nombre:"Talleres Conjunto",
+    categoria: "Primera Division",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+}
+,
+{
+    id:16,
+    nombre:"Banfield Conjunto",
+    categoria: "Primera Division",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+},
+{
+    id:17,
+    nombre:"Gimnasia Mangalarga",
+    categoria: "Primera Division",
+    estilo:"Mangalarga",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+}
+,
 
-// bienvenida="Hola Bienvenido a la pagina de Doble ZZ";
+{
+    id:18,
+    nombre:"Quilmes Conjunto",
+    categoria: "Ascenso",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+}
+,
+{
+    id:19,
+    nombre:"Sacachispas Conjunto",
+    categoria: "Ascenso",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+}
+,
+{
+    id:20,
+    nombre:"Laferrere Conjunto",
+    categoria: "Ascenso",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+},
+{
+    id:21,
+    nombre:"Laferrere Conjunto Heineiken",
+    categoria: "Ascenso",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+}
+,
+{
+    id:22,
+    nombre:"Central Norte Conjunto",
+    categoria: "Ascenso",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+}
+,
+{
+    id:23,
+    nombre:"Claypole Conjunto",
+    categoria: "Ascenso",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+}
+,
+{
+    id:24,
+    nombre:"Chaco For Ever Mangalarga",
+    categoria: "Ascenso",
+    estilo:"Mangalarga",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+}
+,
+{
+    id:25,
+    nombre:"Chacarita Mangalarga",
+    categoria: "Ascenso",
+    estilo:"Mangalarga",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+}
+,
+{
+    id:26,
+    nombre:"Quilmes Mangalarga",
+    categoria: "Ascenso",
+    estilo:"Mangalarga",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+}
+,
+{
+    id:27,
+    nombre:"Alemania Conjunto Heineiken",
+    categoria: "Selecciones",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+}
+,
+{
+    id:28,
+    nombre:"Alemania Conjunto",
+    categoria: "Selecciones",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+}
+,
+{
+    id:29,
+    nombre:"Argentina Mangalarga",
+    categoria: "Selecciones",
+    estilo:"Mangalarga",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+}
+,
+{
+    id:30,
+    nombre:"Argentina negro Conjunto",
+    categoria: "Selecciones",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+}
+,
+{
+    id:31,
+    nombre:"Argentina Conjunto Blanco",
+    categoria: "Selecciones",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+}
+,
+{
+    id:32,
+    nombre:"Fluminense Conjunto",
+    categoria: "Sudamerica",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+}
+,
+{
+    id:33,
+    nombre:"Palmeiras Conjunto Musculosa",
+    categoria: "Sudamerica",
+    estilo:"Musculosa",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+}
+,
+{
+    id:34,
+    nombre:"Flamengo Conjunto Musculosa",
+    categoria: "Sudamerica",
+    estilo:"Musculosa",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+}
+,
+{
+    id:35,
+    nombre:"Fluminense Conjunto Musculosa",
+    categoria: "Sudamerica",
+    estilo:"Musculosa",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+},
+{
+    id:36,
+    nombre:"Real Madrid Conjunto Mangalarga",
+    categoria: "Europa",
+    estilo:"Mangalarga",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+}
+,
+{
+    id:37,
+    nombre:"Real Madrid Conjunto Alternativo",
+    categoria: "Europa",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+}
+,
+{
+    id:38,
+    nombre:"Barcelona Conjunto Mangalarga",
+    categoria: "Europa",
+    estilo:"Mangalarga",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+}
+,
+{
+    id:39,
+    nombre:"Barcelona Conjunto Alternativo",
+    categoria: "Europa",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+}
+,
+{
+    id:40,
+    nombre:"Atletico Madrid Conjunto Mangalarga",
+    categoria: "Europa",
+    estilo:"Mangalarga",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+}
+,
+{
+    id:41,
+    nombre:"Inter De Milan Conjunto Mangalarga",
+    categoria: "Europa",
+    estilo:"Mangalarga",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+}
+,
+{
+    id:42,
+    nombre:"Milan Conjunto",
+    categoria: "Europa",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+}
+,
+{
+    id:43,
+    nombre:"Milan Conjunto Alternativo",
+    categoria: "Europa",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+},
+{
+    id:44,
+    nombre:"Milan Conjunto Alternativo Negro",
+    categoria: "Europa",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+}
+,
+{
+    id:45,
+    nombre:"Napoli Conjunto",
+    categoria: "Europa",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+},
+{
+    id:46,
+    nombre:"Liverpool Conjunto",
+    categoria: "Europa",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+},
+{
+    id:47,
+    nombre:"Los Pibes De Fiorito Conjunto",
+    categoria: "Influencers",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+}
+,
+{
+    id:48,
+    nombre:"Los Pibes De Fiorito Conjunto Musculosa",
+    categoria: "Influencers",
+    estilo:"Musculosa",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+}
+,
+{
+    id:49,
+    nombre:"Los Pibes De Fiorito Mangalarga",
+    categoria: "Influencers",
+    estilo:"Mangalarga",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+}
+,
+{
+    id:50,
+    nombre:"Team Balanza Conjunto",
+    categoria: "Influencers",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+},
+{
+    id:51,
+    nombre:"Team Balanza Conjunto Musculosa",
+    categoria: "Influencers",
+    estilo:"Musculosa",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+}
+,
+{
+    id:52,
+    nombre:"Team Nene Conjunto",
+    categoria: "Influencers",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+},
+{
+    id:53,
+    nombre:"Team Nene Mangalarga",
+    categoria: "Influencers",
+    estilo:"Mangalarga",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+}
+,
+{
+    id:54,
+    nombre:"MDS Conjunto Musculosa",
+    categoria: "Influencers",
+    estilo:"Musculosa",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+}
+,
+{
+    id:55,
+    nombre:"MDS Mangalarga",
+    categoria: "Influencers",
+    estilo:"Mangalarga",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+    
+},
+{
+    id:56,
+    nombre:"Francia Conjunto",
+    categoria: "Selecciones",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+},
+{
+    id:57,
+    nombre:"Brasil Mangalarga",
+    categoria: "Selecciones",
+    estilo:"Mangalarga",
+    precio:14000,
+    talles:"M,L,XL,XXL"
+}
+]
 
-// console.log(bienvenida)
-let nombre=prompt("Ingrese Su Nombre")
-alert("Hola "+nombre+ " Espero que te encuentres bien en la pagina de doble zz")
+
+// Inicio de sesion
+function registroYVerificacion() {
+    // Bucle
+    for(let i = 0; i < 20; i++) {
+        console.log(i);
+    }
+    
+    // Pedimos los datos
+    let usuario = prompt("Ingrese su usuario");
+    let contra = prompt("Ingrese su contraseña");
+    
+    // Arrays
+    const usuarios = [usuario];
+    const contrasenas = [contra];
+    
+    let intentos = 0;
+    let verificacionExitosa = false;
+    
+    // Bucle de verificación con reintentos
+    while (!verificacionExitosa && intentos < 3) {
+        // Vuelva a ingresar
+        let usuarioIngresado = prompt("Ingrese nuevamente su usuario");
+        let contraIngresada = prompt("Ingrese nuevamente su contraseña");
+        
+        // Verificamos
+        if (usuarioIngresado === usuarios[0] && contraIngresada === contrasenas[0]) {
+            alert("Tu contraseña es correcta ✅");
+            verificacionExitosa = true;
+            return true;
+        } else if (usuarioIngresado !== usuarios[0]) {
+            intentos++;
+            if (intentos < 3) {
+                alert("El usuario es incorrecto ❌. Intenta nuevamente.");
+            }
+        } else if (contraIngresada !== contrasenas[0]) {
+            intentos++;
+            if (intentos < 3) {
+                alert("La contraseña es incorrecta ❌. Intenta nuevamente.");
+            }
+        }
+    }
+    
+    // Si agotó los 3 intentos
+    if (!verificacionExitosa) {
+        alert("Has agotado tus intentos. Inténtalo más tarde.");
+        return false;
+    }
+}
+
+// Ejecutar la función
+registroYVerificacion();
+
+    // function Inicio_sesion(){
+    // const password=prompt("Ingrese su contrasena");
+    // let passwordingresado=prompt("Ingrese  su contrasena");
+    // while (passwordingresado  !== password)
+    //     {
+    //     passwordingresado=prompt("Ingrese  su contrasena");
+    //     console.log("su contrasena es incorrecta reintentar")
+    // }
+    //     alert("Has iniciado correctamente la sesión \n Disfruta de la web!");
+    // }   
+    // Inicio_sesion()
+// // CARRRITO PROXIMAMENTE
+
+let carrito=[]
+let total=0
+
